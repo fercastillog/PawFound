9fb115510a0ff25405f0183f4450d360bd362d32
fercastillog <fer.castillog@duocuc.cl>
2025-01-07 16:39:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PawFound',
+  webDir: 'www'
+};
+
+export default config;
