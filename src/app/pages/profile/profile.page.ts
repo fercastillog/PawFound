1051131f0b26ff5ba17b6853ad2d11b6c4d9138e
fercastillog <fer.castillog@duocuc.cl>
2025-01-11 12:35:29 -0300

    implementacion camara(?

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraSource, CameraResultType } from '@capacitor/camera';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
@@ -9,13 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
-
-
-  changeProfilePic(){
+  user = {
+    name: '',
+    lastName: '',
+  };
+  userImage: string | undefined;
+  
+  constructor(private navCtrl: NavController) { 
 
   }
 
+
+  async changeProfilePic() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        source: CameraSource.Camera,
+        resultType: CameraResultType.Base64,
+      });
+  
+      this.userImage = `data:image/jpeg;base64,${image.base64String}`;
+    } catch (error) {
+      console.error('Error al cambiar la foto de perfil:', error);
+    }
+  }
+
+  logout() {
+    console.log('Cerrando sesión...');
+    this.navCtrl.navigateRoot('/login'); // Redirecciónar a la página de inicio de sesión
+     
+  }
   ngOnInit() {
   }
 
