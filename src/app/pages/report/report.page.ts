5352c344dfefda0d62a58c37d5155bcb18fad34f
fercastillog <fer.castillog@duocuc.cl>
2025-01-13 16:39:25 -0300

    algunos ultimos cambios...

--- src/app/pages/report/report.page.ts
+++ src/app/pages/report/report.page.ts
@@ -1,44 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
-
-
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.page.html',
   styleUrls: ['./report.page.scss'],
   standalone: false,
+ 
 })
 export class ReportPage implements OnInit {
-
   reportForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private  navCtrl: NavController) { 
+  constructor(private fb: FormBuilder, private navCtrl: NavController) {
     this.reportForm = this.fb.group({
-    name: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    ubicacion: ['', Validators.required],
-    datetime: ['', Validators.required],
-    phone: ['', Validators.required, Validators.pattern('[0-9]{9}')],
-    animal: ['', Validators.required],
-    sexo:['', Validators.required],
-    description: ['', Validators.required, Validators.maxLength(100)],
-    photo: ['', Validators.required]
-
-
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      ubicacion: ['', Validators.required],
+      datetime: [''],
+      phone: ['', [Validators.required, Validators.pattern('[0-9]{9}')]],
+      animal: ['', Validators.required],
+      sexo: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(100)]],
+      photo: ['', Validators.required],
     });
   }
 
   ngOnInit() {
+    console.log('Estado inicial del formulario:', this.reportForm.status);
   }
 
-  onReport(){
-    if(this.reportForm.valid){
-      const { name, ubicacion, datetime, phone, animal, sexo, description, photo } = this.reportForm.value;
-      console.log('Report: ',name, ubicacion, phone, datetime, animal, sexo, description, photo);
-      // Aquí se llamará al AuthService para manejar la autenticación
-      // Redireccionara a la pagina home o where ever...
+  onReport() {
+    if (this.reportForm.valid) {
+      console.log('Datos enviados:', this.reportForm.value);
+      this.reportForm.reset(); // Vacía el formulario después de enviar
+    } else {
+      console.log('Formulario inválido');
     }
   }
-
 }
